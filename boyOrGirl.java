ed1b8832a20f750694ae2147da56e5c2f64f9e93
dexterzPK <dontwanttolive945@gmail.com>
2024-07-31 01:00:23 +0530

    boy or girl problem

---
+++ boyOrGirl.java
@@ -0,0 +1,22 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class boyOrGirl {
+    public static void main(String []args) {
+        @SuppressWarnings("resource")
+        Scanner sc = new Scanner(System.in);
+        Set<Character> res = new HashSet<>();
+        String s = sc.next();
+        s.toLowerCase();
+        for(int i = 0; i < s.length(); i++) {
+            res.add(s.charAt(i));
+        }
+        if(res.size() % 2 == 0) {
+            System.out.println("CHAT WITH HER!");
+        }
+        else {
+            System.out.println("IGNORE HIM!");
+        }
+    }
+}
