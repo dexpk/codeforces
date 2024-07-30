14de37ccf4e5dd0ec769cd9b2b2bed96a3a0a7d4
dexterzPK <dontwanttolive945@gmail.com>
2024-07-31 01:11:23 +0530

    bear big brother weight problem

---
+++ bearBigBrother.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class bearBigBrother {
+    public static void main(String[] args) {
+        @SuppressWarnings("resource")
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int year = 0;
+        for(int i = 0; i < 100; i++) {
+            if(a>b) {
+                System.out.println(year);
+                break;
+            }
+            else {
+                a = a * 3;
+                b = b * 2;
+                year ++;
+            }
+        }
+    }
+}
